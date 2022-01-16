63f9beaca112dd8d808f3648726d597ec9ca5134
david <david@HP-470-DAVID>
2022-01-16 16:50:12 +0100

    modulo 4

--- XAMLInXamarinForms/XAMLInXamarinForms/Views/NavigationPageView.xaml.cs
+++ XAMLInXamarinForms/XAMLInXamarinForms/Views/NavigationPageView.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XAMLInXamarinForms.ViewModels;
+using XAMLInXamarinForms.Views.Fitness;
+using XAMLInXamarinForms.Views.Nutrition;
 
 namespace XAMLInXamarinForms.Views
 {
@@ -15,5 +16,17 @@ namespace XAMLInXamarinForms.Views
         {
             InitializeComponent();
         }
+
+        private async void FitnessButton_Clicked(object sender, EventArgs e)
+        {
+            await this.Navigation.PushAsync(new FitnessView());
+        }
+
+        private async void NutritionButton_Clicked(object sender, EventArgs e)
+        {
+            await this.Navigation.PushAsync(new NutritionView());
+        }
+
+
     }
 }
